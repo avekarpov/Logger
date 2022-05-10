55efaadb7e945ae2be7b5c160e01f9ceaf5fc4d0
Artem Karpov <artem.ka1001pm@gmail.com>
2022-05-10 16:21:34 +0300

    Add capture cout class

--- test/main.cpp
+++ test/main.cpp
@@ -2,6 +2,36 @@
 #include <catch2/catch.hpp>
 #include <logger.hpp>
 
+class CaptureCout
+{
+public:
+    explicit CaptureCout(std::ostream &stream = std::cout) :
+    _stream(stream)
+    {
+        _sbuf = _stream.rdbuf();
+        _stream.rdbuf(_ss.rdbuf());
+    }
+    
+    ~CaptureCout()
+    {
+        _stream.rdbuf(_sbuf);
+    }
+    
+    std::string data() const
+    {
+        return _ss.str();
+    }
+    
+    CaptureCout(const CaptureCout &) = delete;
+    CaptureCout &operator=(const CaptureCout &) = delete;
+
+private:
+    std::stringstream _ss;
+    std::streambuf *_sbuf;
+    std::ostream &_stream;
+
+};
+
 TEST_CASE("Logger")
 {
 
